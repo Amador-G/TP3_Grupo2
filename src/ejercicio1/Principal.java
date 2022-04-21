28e7a62a602eba7cf284415287399823f35b6d00
JorgeCampostanoFroiman <67806498+JorgeCampostanoFroiman@users.noreply.github.com>
2022-04-21 18:56:27 -0300

    Agrego clase archivo y leo el txt

--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -2,11 +2,12 @@ package ejercicio1;
 
 import javax.swing.JOptionPane;
 
+
 public class Principal {
 
 	public static void main(String[] args) {
 
-		try
+		/*try
 		{
 			String dni = JOptionPane.showInputDialog("Ingrese su Numero de Dni: ");
 			Dni.VerificarDniInvalido(dni);
@@ -16,7 +17,34 @@ public class Principal {
 		{
 			System.out.println("Debe Tener solo numeros!");
 			e.printStackTrace();
+		}*/
+		
+		
+		Archivo archivo = new Archivo();
+		archivo.setRuta("Personas.txt");
+		
+		if(archivo.existe())
+		{
+			System.out.println("Existe archivo");
+			
+			archivo.lee_lineas();
+			
+			
 		}
+		else
+		{
+			System.out.println("No existe archivo.");
+		}
+		
+		
 	}
+	
+	
+	
+	
+
+	
+	
+	
 
 }
